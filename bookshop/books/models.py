abcf9b4ebd7686fa8428760e658d750f5bd30199
Paul Wolf <paul.wolf@yewleaf.com>
2020-04-26 07:41:40 +0100

    Add user profile

--- bookshop/books/models.py
+++ bookshop/books/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    company = models.CharField(max_length=64, null=True, blank=True)
+
+    def __str__(self):
+        return "{self.user.username} ({self.company})"
 
 
 class Author(models.Model):
