45d8fe099981b84283287170fa2644104602382b
Paul Wolf <paul.wolf@yewleaf.com>
2020-04-28 09:51:19 +0100

    Remove unused import

--- bookshop/bookshop/urls.py
+++ bookshop/bookshop/urls.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
 from django.urls import path, re_path, include
 
-from djaq.ui import urls as djaq_urls
-
 urlpatterns = [path("admin/", admin.site.urls), path("query/", include("djaq.ui.urls"))]
